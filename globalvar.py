d8ea13c81ab3f078565ef8200aa19b7bce74137a
kainan <kainanos@outlook.com>
2022-07-28 21:49:23 +0800

    20220728

--- globalvar.py
+++ globalvar.py
@@ -6,7 +6,8 @@ GuiInfo = {"proj": "PyCOM",
            "cwd": ""
            }
 
-SerialInfo = {"baudrate": ['2400', '4800', '9600', '14400', '19200', '38400', '57600', '76800', '115200', '128000', '230400', '256000', '460800', '921600', '1000000'],
+SerialInfo = {"baudrate": ['300', '600', '1200', '2400', '4800', '9600', '14400', '19200', '38400', '57600', '76800',
+                           '115200', '128000', '230400', '256000', '460800', '921600', '1000000', '2000000', '3000000'],
               "bytesize": ['8', '7', '6', '5'],
               "stopbit": ['1', '2'],
               "paritybit": ['None', 'Odd', 'Even'],
@@ -18,5 +19,5 @@ AboutInfo = f"""
     Version: {GuiInfo["version"]}
 
     Support: {GuiInfo["email"]}
-    Repo: https://github.com/kainan2020/PyCom
+    Github Repo: https://github.com/kainan2020/PyCom
 """
